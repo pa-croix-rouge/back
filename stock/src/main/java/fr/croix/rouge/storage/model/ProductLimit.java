7a9bf8b443aa9ac86eb291f973ed718fbe0a07a8
Clément <clementhottotpro@gmail.com>
2023-04-02 22:30:34 +0200

    Fix compilation

--- stock/src/main/java/fr/croix/rouge/storage/model/ProductLimit.java
+++ stock/src/main/java/fr/croix/rouge/storage/model/ProductLimit.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 public class ProductLimit {
 
+    public static final ProductLimit NO_LIMIT = new ProductLimit(null, null);
+
     private final Duration duration;
     private final Quantifier quantity;
 
@@ -15,6 +17,8 @@ public class ProductLimit {
     }
 
     public boolean isLimitReached(List<StorageUserProduct> products) {
+        if (duration == null || quantity == null) return false;
+
         LocalDate start = LocalDate.now().minusDays(duration.toDays());
 
         List<Quantifier> q = products.stream().filter(p -> p.date().isAfter(start)).map(sup -> sup.product().Quantity).toList();
--- web-api/src/main/java/fr/croixrouge/service/FoodProductService.java
+++ web-api/src/main/java/fr/croixrouge/service/FoodProductService.java
@@ -3,11 +3,13 @@ package fr.croixrouge.service;
 import fr.croix.rouge.storage.model.FoodProduct;
 import fr.croix.rouge.storage.model.LiquidQuantifier;
 import fr.croix.rouge.storage.model.LiquidUnit;
+import fr.croix.rouge.storage.model.ProductLimit;
+import fr.croixrouge.domain.model.ID;
 
 public class FoodProductService {
 
     FoodProductService() {
-        FoodProduct foodProduct = new FoodProduct(new FoodProduct.ID("1"), "name", new LiquidQuantifier(1, LiquidUnit.LITER));
+        FoodProduct foodProduct = new FoodProduct(new ID("1"), "name", new LiquidQuantifier(1, LiquidUnit.LITER), ProductLimit.NO_LIMIT);
     }
 
 }
