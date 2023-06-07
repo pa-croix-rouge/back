98a3709f3128249fdeb18e4462208331c3337117
Quentin MOLERO <quentin.molero@outlook.com>
2023-06-07 12:48:05 +0200

    Update CI to run test in db

--- stock/src/main/java/fr/croixrouge/storage/repository/memory/InMemoryProductRepository.java
+++ stock/src/main/java/fr/croixrouge/storage/repository/memory/InMemoryProductRepository.java
@@ -3,13 +3,11 @@ package fr.croixrouge.storage.repository.memory;
 import fr.croixrouge.domain.model.ID;
 import fr.croixrouge.domain.repository.InMemoryCRUDRepository;
 import fr.croixrouge.domain.repository.TimeStampIDGenerator;
-import fr.croixrouge.storage.model.product.FoodProduct;
 import fr.croixrouge.storage.model.product.Product;
 import fr.croixrouge.storage.repository.ProductRepository;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class InMemoryProductRepository extends InMemoryCRUDRepository<ID, Product> implements ProductRepository {
 
@@ -19,15 +17,5 @@ public class InMemoryProductRepository extends InMemoryCRUDRepository<ID, Produc
 
     public InMemoryProductRepository() {
         super(new ArrayList<>(), new TimeStampIDGenerator());
-    }
-
-    @Override
-    public Optional<FoodProduct> findByIdFood(ID id) {
-        return findById(id).map( product -> {
-            if (product instanceof FoodProduct) {
-                return (FoodProduct) product;
-            }
-            return null;
-        });
     }
 }
