bfd007326a2d3287c23f725a2024544666c03082
Quentin MOLERO <quentin.molero@outlook.com>
2023-06-12 11:01:00 +0200

    Fix event session db query

--- repository/src/main/java/fr/croixrouge/repository/db/event/EventSessionDBRepository.java
+++ repository/src/main/java/fr/croixrouge/repository/db/event/EventSessionDBRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface EventSessionDBRepository extends CrudRepository<EventSessionDB, Long> {
-    @Query("select e from EventSessionDB e where e.eventDB.localUnitDB.localUnitID = ?1 and e.eventDB.startTime >= ?2")
+    @Query("select e from EventSessionDB e where e.eventDB.localUnitDB.localUnitID = ?1 and e.start >= ?2")
     List<EventSessionDB> findByEventDB_LocalUnitDB_LocalUnitIDAndStartTimeAfter(Long localUnitID, ZonedDateTime startTime);
 
     @Query("select e from EventSessionDB e where e.eventDB.id = ?1")
@@ -17,6 +17,6 @@ public interface EventSessionDBRepository extends CrudRepository<EventSessionDB,
     @Query("select e from EventSessionDB e where e.id = ?1 and e.eventDB.id = ?2")
     Optional<EventSessionDB> findByIdAndEventDB_Id(Long id, Long id1);
 
-    @Query("select e from EventSessionDB e where e.eventDB.localUnitDB.localUnitID = ?1 and e.eventDB.startTime >= ?2 and e.eventDB.endTime <= ?3")
+    @Query("select e from EventSessionDB e where e.eventDB.localUnitDB.localUnitID = ?1 and e.start >= ?2 and e.end <= ?3")
     List<EventSessionDB> findByLocalUnitDB_LocalUnitIDAndStartTimeAfterOrEndTimeBefore(Long localUnitID, ZonedDateTime startTime, ZonedDateTime endTime);
 }
