99c9bf701adda59800a2d187f4fb9b61be8a6fdc
Paul Abib <paul_abib@connect-tech.sncf>
2023-06-26 08:48:04 +0200

    add / to login controller

--- web-api/src/main/java/fr/croixrouge/exposition/controller/LoginController.java
+++ web-api/src/main/java/fr/croixrouge/exposition/controller/LoginController.java
@@ -25,8 +25,9 @@ public class LoginController extends ErrorHandler {
         this.service = service;
     }
 
-    @PostMapping("volunteer")
+    @PostMapping("/volunteer")
     public ResponseEntity<LoginResponse> volunteerLogin(@RequestBody LoginRequest loginRequest) {
+        System.out.println("volunteerLogin");
         try { // TODO Controller Exception handling
             return ResponseEntity.ok(service.authenticateVolunteer(loginRequest.getUsername(), loginRequest.getPassword()));
         } catch (AuthenticationException e) {
@@ -34,7 +35,7 @@ public class LoginController extends ErrorHandler {
         }
     }
 
-    @PostMapping("beneficiary")
+    @PostMapping("/beneficiary")
     public ResponseEntity<LoginResponse> beneficiaryLogin(@RequestBody LoginRequest loginRequest) {
         try { // TODO Controller Exception handling
             return ResponseEntity.ok(service.authenticateBeneficiary(loginRequest.getUsername(), loginRequest.getPassword()));
