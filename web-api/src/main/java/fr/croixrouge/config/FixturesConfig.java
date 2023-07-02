8afe618742cbf5255ae2d3ce4590f5857e6f3f00
Clément <clementhottotpro@gmail.com>
2023-07-02 13:56:12 +0200

    Added limitProduct to fixtures config

--- web-api/src/main/java/fr/croixrouge/config/FixturesConfig.java
+++ web-api/src/main/java/fr/croixrouge/config/FixturesConfig.java
@@ -44,10 +44,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.time.*;
 import java.util.*;
 
 @Configuration
@@ -63,6 +60,8 @@ public class FixturesConfig {
     private final LocalUnit localUnit;
 
     private final Product cloth1, cloth2, cloth3, cloth4, cloth5, cloth6, cloth7, cloth8, cloth9, cloth10, cloth11, cloth12, cloth13, cloth14, cloth15, food1, food2, food3, food4, food5, food6, food7, food8, food9, food10, food11, food12, food13, food14, food15;
+
+    private final List<ProductLimit> productLimits;
 
     public FixturesConfig(PasswordEncoder passwordEncoder) {
         localUnit = new LocalUnit(new ID(1L),
@@ -221,36 +220,54 @@ public class FixturesConfig {
         beneficiary46 = new Beneficiary(null, beneficiaryUserValOrge46, "Zola", "Ndlovu", "+263 753 628 841", true, LocalDate.of(1994, 5, 25), "2 94 05 25 91 157 633", List.of());
         beneficiary47 = new Beneficiary(null, beneficiaryUserValOrge47, "Sibusiso", "Khumalo", "+263 437 845 219", true, LocalDate.of(1989, 12, 15), "1 89 12 15 91 143 751", List.of());
 
-        cloth1 = new Product(null, "Chemises blanches", new Quantifier(20, NumberedUnit.NUMBER), null);
-        cloth2 = new Product(null, "Chemises blanches", new Quantifier(15, NumberedUnit.NUMBER), null);
-        cloth3 = new Product(null, "Chemises blanches", new Quantifier(17, NumberedUnit.NUMBER), null);
-        cloth4 = new Product(null, "Chemises blanches", new Quantifier(6, NumberedUnit.NUMBER), null);
-        cloth5 = new Product(null, "Chemises blanches", new Quantifier(5, NumberedUnit.NUMBER), null);
-        cloth6 = new Product(null, "T-shirts blancs", new Quantifier(15, NumberedUnit.NUMBER), null);
-        cloth7 = new Product(null, "Pantalons noirs", new Quantifier(10, NumberedUnit.NUMBER), null);
-        cloth8 = new Product(null, "Robes rouges", new Quantifier(8, NumberedUnit.NUMBER), null);
-        cloth9 = new Product(null, "Pulls gris", new Quantifier(12, NumberedUnit.NUMBER), null);
-        cloth10 = new Product(null, "Jupes bleues", new Quantifier(20, NumberedUnit.NUMBER), null);
-        cloth11 = new Product(null, "Chaussures noires", new Quantifier(5, NumberedUnit.NUMBER), null);
-        cloth12 = new Product(null, "Chaussures blanches", new Quantifier(8, NumberedUnit.NUMBER), null);
-        cloth13 = new Product(null, "Chaussettes noires", new Quantifier(10, NumberedUnit.NUMBER), null);
-        cloth14 = new Product(null, "Chaussettes blanches", new Quantifier(15, NumberedUnit.NUMBER), null);
-        cloth15 = new Product(null, "Baskets rouges", new Quantifier(7, NumberedUnit.NUMBER), null);
-        food1 = new Product(null, "Pommes", new WeightQuantifier(1, WeightUnit.KILOGRAM), null);
-        food2 = new Product(null, "Pates", new WeightQuantifier(1, WeightUnit.KILOGRAM), null);
-        food3 = new Product(null, "Bananes", new WeightQuantifier(500, WeightUnit.GRAM), null);
-        food4 = new Product(null, "Oranges", new WeightQuantifier(750, WeightUnit.GRAM), null);
-        food5 = new Product(null, "Tomates", new WeightQuantifier(1, WeightUnit.KILOGRAM), null);
-        food6 = new Product(null, "Carottes", new WeightQuantifier(1, WeightUnit.KILOGRAM), null);
-        food7 = new Product(null, "Pommes de terre", new WeightQuantifier(2, WeightUnit.KILOGRAM), null);
-        food8 = new Product(null, "Fraises", new WeightQuantifier(250, WeightUnit.GRAM), null);
-        food9 = new Product(null, "Blancs de poulet", new WeightQuantifier(500, WeightUnit.GRAM), null);
-        food10 = new Product(null, "Filets de saumon", new WeightQuantifier(1, WeightUnit.KILOGRAM), null);
-        food11 = new Product(null, "Épinards", new WeightQuantifier(200, WeightUnit.GRAM), null);
-        food12 = new Product(null, "Yaourt", new WeightQuantifier(500, WeightUnit.GRAM), null);
-        food13 = new Product(null, "Pain", new WeightQuantifier(800, WeightUnit.GRAM), null);
-        food14 = new Product(null, "Viande hachée", new WeightQuantifier(1, WeightUnit.KILOGRAM), null);
-        food15 = new Product(null, "Oignons", new WeightQuantifier(750, WeightUnit.GRAM), null);
+        var productLimit = new ProductLimit(null, "Farine", Duration.ofDays(15), new Quantifier(1, WeightUnit.KILOGRAM));
+        var productLimit1 = new ProductLimit(null, "Sucre", Duration.ofDays(15), new Quantifier(1, WeightUnit.KILOGRAM));
+        var productLimit2 = new ProductLimit(null, "Couche", Duration.ofDays(15), new Quantifier(1, NumberedUnit.NUMBER));
+        var productLimit3 = new ProductLimit(null, "Lessive", Duration.ofDays(30), new Quantifier(1, VolumeUnit.LITER));
+        var productLimit14 = new ProductLimit(null, "laitier", Duration.ofDays(30), new Quantifier(1, VolumeUnit.LITER));
+        var productLimit4 = new ProductLimit(null, "Brosse a dents", Duration.ofDays(60), new Quantifier(1, NumberedUnit.NUMBER));
+        var productLimit5 = new ProductLimit(null, "dentifrice", Duration.ofDays(30), new Quantifier(1, NumberedUnit.NUMBER));
+        var productLimit6 = new ProductLimit(null, "shampooing / gel douche", Duration.ofDays(15), new Quantifier(1, VolumeUnit.LITER));
+        var productLimit7 = new ProductLimit(null, "féculents", Duration.ofDays(15), new Quantifier(1, WeightUnit.KILOGRAM));
+        var productLimit8 = new ProductLimit(null, "légumes", Duration.ofDays(15), new Quantifier(1, WeightUnit.KILOGRAM));
+        var productLimit13 = new ProductLimit(null, "viandes", Duration.ofDays(15), new Quantifier(1, WeightUnit.KILOGRAM));
+        var productLimit9 = new ProductLimit(null, "fruits", Duration.ofDays(15), new Quantifier(1, WeightUnit.KILOGRAM));
+        var productLimit10 = new ProductLimit(null, "sous vêtements", Duration.ofDays(30), new Quantifier(5, NumberedUnit.NUMBER));
+        var productLimit11 = new ProductLimit(null, "vêtements", Duration.ofDays(30), new Quantifier(5, NumberedUnit.NUMBER));
+        var productLimit12 = new ProductLimit(null, "sur vêtements", Duration.ofDays(30), new Quantifier(5, NumberedUnit.NUMBER));
+
+        productLimits = List.of(productLimit, productLimit1, productLimit2, productLimit3, productLimit4, productLimit5, productLimit6, productLimit7, productLimit8, productLimit9, productLimit10, productLimit11, productLimit12, productLimit13, productLimit14);
+
+        cloth1 = new Product(null, "Chemises blanches", new Quantifier(20, NumberedUnit.NUMBER), productLimit11);
+        cloth2 = new Product(null, "Chemises blanches", new Quantifier(15, NumberedUnit.NUMBER), productLimit11);
+        cloth3 = new Product(null, "Chemises blanches", new Quantifier(17, NumberedUnit.NUMBER), productLimit11);
+        cloth4 = new Product(null, "Chemises blanches", new Quantifier(6, NumberedUnit.NUMBER), productLimit11);
+        cloth5 = new Product(null, "Chemises blanches", new Quantifier(5, NumberedUnit.NUMBER), productLimit11);
+        cloth6 = new Product(null, "T-shirts blancs", new Quantifier(15, NumberedUnit.NUMBER), productLimit11);
+        cloth7 = new Product(null, "Pantalons noirs", new Quantifier(10, NumberedUnit.NUMBER), productLimit11);
+        cloth8 = new Product(null, "Robes rouges", new Quantifier(8, NumberedUnit.NUMBER), productLimit11);
+        cloth9 = new Product(null, "Pulls gris", new Quantifier(12, NumberedUnit.NUMBER), productLimit11);
+        cloth10 = new Product(null, "Jupes bleues", new Quantifier(20, NumberedUnit.NUMBER), productLimit11);
+        cloth11 = new Product(null, "Chaussures noires", new Quantifier(5, NumberedUnit.NUMBER), productLimit12);
+        cloth12 = new Product(null, "Chaussures blanches", new Quantifier(8, NumberedUnit.NUMBER), productLimit12);
+        cloth13 = new Product(null, "Chaussettes noires", new Quantifier(10, NumberedUnit.NUMBER), productLimit10);
+        cloth14 = new Product(null, "Chaussettes blanches", new Quantifier(15, NumberedUnit.NUMBER), productLimit10);
+        cloth15 = new Product(null, "Baskets rouges", new Quantifier(7, NumberedUnit.NUMBER), productLimit12);
+        food1 = new Product(null, "Pommes", new WeightQuantifier(1, WeightUnit.KILOGRAM), productLimit9);
+        food2 = new Product(null, "Pates", new WeightQuantifier(1, WeightUnit.KILOGRAM), productLimit7);
+        food3 = new Product(null, "Bananes", new WeightQuantifier(500, WeightUnit.GRAM), productLimit9);
+        food4 = new Product(null, "Oranges", new WeightQuantifier(750, WeightUnit.GRAM), productLimit9);
+        food5 = new Product(null, "Tomates", new WeightQuantifier(1, WeightUnit.KILOGRAM), productLimit9);
+        food6 = new Product(null, "Carottes", new WeightQuantifier(1, WeightUnit.KILOGRAM), productLimit8);
+        food7 = new Product(null, "Pommes de terre", new WeightQuantifier(2, WeightUnit.KILOGRAM), productLimit7);
+        food8 = new Product(null, "Fraises", new WeightQuantifier(250, WeightUnit.GRAM), productLimit9);
+        food9 = new Product(null, "Blancs de poulet", new WeightQuantifier(500, WeightUnit.GRAM), productLimit13);
+        food10 = new Product(null, "Filets de saumon", new WeightQuantifier(1, WeightUnit.KILOGRAM), productLimit13);
+        food11 = new Product(null, "Épinards", new WeightQuantifier(200, WeightUnit.GRAM), productLimit8);
+        food12 = new Product(null, "Yaourt", new WeightQuantifier(500, WeightUnit.GRAM), productLimit14);
+        food13 = new Product(null, "Pain", new WeightQuantifier(800, WeightUnit.GRAM), productLimit7);
+        food14 = new Product(null, "Viande hachée", new WeightQuantifier(1, WeightUnit.KILOGRAM), productLimit13);
+        food15 = new Product(null, "Oignons", new WeightQuantifier(750, WeightUnit.GRAM), productLimit8);
     }
 
     @Bean
@@ -434,7 +451,13 @@ public class FixturesConfig {
     @Bean
     @Primary
     public InDBProductLimitRepository productLimitTestRepository(ProductLimitDBRepository productLimitDBRepository) {
-        return new InDBProductLimitRepository(productLimitDBRepository);
+        var repo = new InDBProductLimitRepository(productLimitDBRepository);
+
+        for (var productLimit : productLimits) {
+            repo.save(productLimit);
+        }
+
+        return repo;
     }
 
     @Bean
@@ -445,7 +468,7 @@ public class FixturesConfig {
 
     @Bean
     @Primary
-    public InDBClothProductRepository clothProductRepository(ClothProductDBRepository clothProductDBRepository, InDBProductRepository productRepository) {
+    public InDBClothProductRepository clothProductRepository(ClothProductDBRepository clothProductDBRepository, InDBProductRepository productRepository, InDBProductLimitRepository productLimitRepository) {
         InDBClothProductRepository repository = new InDBClothProductRepository(clothProductDBRepository, productRepository);
 
         repository.save(new ClothProduct(new ID(1L), cloth1, ClothSize.S, ClothGender.NOT_SPECIFIED));
